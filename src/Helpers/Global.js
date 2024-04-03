0cfe82fa996de45f8ebee7f091fac84cd4d99c73
unknown <lautarorcloudy@gmail.com>
2024-04-03 15:17:32 -0300

    update

--- src/Helpers/Global.js
+++ src/Helpers/Global.js
@@ -1,6 +1,6 @@
 const Global =  {
-    url: "http://localhost:3000/api/"
-    // url:"https://api-rest-usuarios.vercel.app/api/"
+    // url: "http://localhost:3000/api/"
+    url:"https://ai-server-back.vercel.app/api/"
    }
    
    export default Global
